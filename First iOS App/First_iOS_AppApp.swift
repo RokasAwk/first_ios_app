55192d4354dfd5316079f3133821a05c92f7c412
Rokas <icestar6215@gmail.com>
2024-10-14 10:47:58 +0800

    Initial Commit

---
+++ First iOS App/First_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_iOS_AppApp.swift
+//  First iOS App
+//
+//  Created by 李翗祤 on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct First_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
