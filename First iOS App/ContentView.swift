d9425623b52b95a64065c9385b22849c8f07d4d3
Rokas <icestar6215@gmail.com>
2024-10-14 13:16:07 +0800

    feat: learn X,Y,Z Stack and some decorators

--- First iOS App/ContentView.swift
+++ First iOS App/ContentView.swift
@@ -1,24 +1,33 @@
-//
-//  ContentView.swift
-//  First iOS App
-//
-//  Created by 李翗祤 on 2024/10/14.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color
+                .blue.opacity(0.5)
+                .ignoresSafeArea() // 滿版
+            
+            VStack {
+                Image(systemName: "pawprint.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Welcome to iOS app")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.semibold)
+                Text("TAP")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .padding()
+                    .background(Color.gray.opacity(0.5))
+             
+            }
+            .padding()
         }
-        .padding()
     }
 }
-
+ 
 #Preview {
     ContentView()
 }
