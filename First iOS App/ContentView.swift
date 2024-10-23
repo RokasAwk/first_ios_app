627d218d7e5a3e4ffb7fd34d581013ce1082e959
Rokas <icestar6215@gmail.com>
2024-10-23 17:16:50 +0800

    feat: learn how to use AVFoundation

--- First iOS App/ContentView.swift
+++ First iOS App/ContentView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
+import AVFoundation // 文字轉語音
 
 struct ContentView: View {
+    
+    let synthesizer = AVSpeechSynthesizer()
+    let helloWorld: String = "Hello World"
+    let helloProgrammer: String = "Hello Programmer"
+    
     var body: some View {
         ZStack {
             Color
@@ -16,18 +22,56 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .multilineTextAlignment(.center)
                     .fontWeight(.semibold)
-                Text("TAP")
-                    .font(.title3)
-                    .fontWeight(.light)
-                    .padding()
-                    .background(Color.gray.opacity(0.5))
+               
+                Button {
+                    // What to perform
+                    speak(text: helloWorld)
+                } label: {
+                    // How the button looks like
+                    Text(helloWorld)
+                        .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(Color.gray.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                    
+                }
+                
+                Button {
+                    // What to perform
+                    speak(text: helloProgrammer)
+                } label: {
+                    // How the button looks like
+                    Text(helloProgrammer)
+                        .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
+                        .fontWeight(.bold)
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(Color.yellow.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                    
+                }
              
             }
             .padding()
         }
     }
+    
+    
+    func speak(text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.speech.synthesis.voice.Fred")
+
+        synthesizer.speak(utterance)
+    }
 }
- 
+
+
 #Preview {
     ContentView()
 }
+
+
+
+
