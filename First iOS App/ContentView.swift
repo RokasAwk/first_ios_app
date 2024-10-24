ca7372767b6746448f69e987164d384f343822e3
Rokas <icestar6215@gmail.com>
2024-10-24 17:53:43 +0800

    feat: learn extract subview and rotate screen to Preview

--- First iOS App/ContentView.swift
+++ First iOS App/ContentView.swift
@@ -1,11 +1,10 @@
 import SwiftUI
-import AVFoundation // 文字轉語音
 
 struct ContentView: View {
     
-    let synthesizer = AVSpeechSynthesizer()
     let helloWorld: String = "Hello World"
     let helloProgrammer: String = "Hello Programmer"
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     
     var body: some View {
         ZStack {
@@ -13,77 +12,131 @@ struct ContentView: View {
                 .blue.opacity(0.5)
                 .ignoresSafeArea() // 滿版
             
-            VStack {
+            VStack (spacing: 20) {
                 VStack {
-                    Image(systemName: "pawprint.fill")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    Text("Welcome to iOS app")
-                        .foregroundColor(Color.white)
-                        .font(.largeTitle)
-                        .multilineTextAlignment(.center)
-                        .fontWeight(.semibold)
+                    Title()
                     Text("Make sure you are ready")
                         .foregroundColor(Color.white)
                         .font(.system(size: 24))
                         .multilineTextAlignment(.center)
                         .fontWeight(.semibold)
                     
-                    Button {
-                        // What to perform
-                        speak(text: helloWorld)
-                    } label: {
-                        // How the button looks like
-                        Text(helloWorld)
-                            .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
-                            .fontWeight(.bold)
-                            .padding()
-                            .foregroundStyle(.white)
-                            .background(Color.gray.opacity(0.5))
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        
-                    }
-                    
-                    Button {
-                        // What to perform
-                        speak(text: helloProgrammer)
-                    } label: {
-                        // How the button looks like
-                        Text(helloProgrammer)
-                            .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
-                            .fontWeight(.bold)
-                            .padding()
-                            .foregroundStyle(.white)
-                            .background(Color.yellow.opacity(0.5))
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                        
-                    }
                     
                 }
                 .padding()
                 
+                
+                UserImg()
+                
+                Text("Need help with coding problems? Register!")
+                
                 Spacer()
+                
+                if verticalSizeClass == .compact {
+                    HSignUpButtonGroup()
+                } else {
+                    VSignUpButtonGroup()
+                }
+                                
+                
             }
             .padding(.top, 30)
-            
-            
+           
         }
     }
     
-    
-    func speak(text: String) {
-        let utterance = AVSpeechUtterance(string: text)
-        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.speech.synthesis.voice.Fred")
+}
 
-        synthesizer.speak(utterance)
+
+struct VSignUpButtonGroup: View {
+    var body: some View {
+        VStack{
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.indigo)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Button {
+                
+            } label: {
+                Text("Log In")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 10))}
     }
 }
 
+struct HSignUpButtonGroup: View {
+    var body: some View {
+        HStack{
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.indigo)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Button {
+                
+            } label: {
+                Text("Log In")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 10))}
+    }
+}
 
-#Preview {
+struct UserImg: View {
+    var body: some View {
+        HStack(alignment: .bottom,
+               spacing: 10)
+        {
+            Image("user1")
+                .resizable()
+                .scaledToFit()
+            
+            Image("user2")
+                .resizable()
+                .scaledToFit()
+            
+            Image("user3")
+                .resizable()
+                .scaledToFit()
+        }
+        .padding(.horizontal,30)
+    }
+}
+
+struct Title: View {
+    var body: some View {
+        Text("Welcome to iOS app")
+            .foregroundColor(Color.white)
+            .font(.largeTitle)
+            .multilineTextAlignment(.center)
+            .fontWeight(.semibold)
+    }
+}
+
+#Preview("ContentView") {
     ContentView()
 }
 
-
-
-
+#Preview("ContentView (Landscape Left)",traits: .landscapeLeft) {
+    ContentView()
+}
