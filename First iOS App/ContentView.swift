35074a180d6c2891dbbbeaee138576f4e636fc96
Rokas <icestar6215@gmail.com>
2024-10-24 09:56:33 +0800

    feat: learn how to use spacer

--- First iOS App/ContentView.swift
+++ First iOS App/ContentView.swift
@@ -14,47 +14,59 @@ struct ContentView: View {
                 .ignoresSafeArea() // 滿版
             
             VStack {
-                Image(systemName: "pawprint.fill")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Welcome to iOS app")
-                    .foregroundColor(Color.white)
-                    .font(.largeTitle)
-                    .multilineTextAlignment(.center)
-                    .fontWeight(.semibold)
-               
-                Button {
-                    // What to perform
-                    speak(text: helloWorld)
-                } label: {
-                    // How the button looks like
-                    Text(helloWorld)
-                        .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
-                        .fontWeight(.bold)
-                        .padding()
-                        .foregroundStyle(.white)
-                        .background(Color.gray.opacity(0.5))
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                VStack {
+                    Image(systemName: "pawprint.fill")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Welcome to iOS app")
+                        .foregroundColor(Color.white)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.semibold)
+                    Text("Make sure you are ready")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 24))
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.semibold)
+                    
+                    Button {
+                        // What to perform
+                        speak(text: helloWorld)
+                    } label: {
+                        // How the button looks like
+                        Text(helloWorld)
+                            .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
+                            .fontWeight(.bold)
+                            .padding()
+                            .foregroundStyle(.white)
+                            .background(Color.gray.opacity(0.5))
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                        
+                    }
+                    
+                    Button {
+                        // What to perform
+                        speak(text: helloProgrammer)
+                    } label: {
+                        // How the button looks like
+                        Text(helloProgrammer)
+                            .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
+                            .fontWeight(.bold)
+                            .padding()
+                            .foregroundStyle(.white)
+                            .background(Color.yellow.opacity(0.5))
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                        
+                    }
                     
                 }
+                .padding()
                 
-                Button {
-                    // What to perform
-                    speak(text: helloProgrammer)
-                } label: {
-                    // How the button looks like
-                    Text(helloProgrammer)
-                        .font(.system(.title3, design: .rounded))                    .fontWeight(.bold)
-                        .fontWeight(.bold)
-                        .padding()
-                        .foregroundStyle(.white)
-                        .background(Color.yellow.opacity(0.5))
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
-                    
-                }
-             
+                Spacer()
             }
-            .padding()
+            .padding(.top, 30)
+            
+            
         }
     }
     
